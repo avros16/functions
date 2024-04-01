6034efbf401d3501e4451612ae1ea988cfba6142
avros16 <114757828+avros16@users.noreply.github.com>
2024-04-01 12:57:06 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@function/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+// functions always have parenthasis. Paramteres go inside the (). 
+  // floor(5.2) = 5
+  // ceil(5.2) = 6
+  
+  std::cout << "Hello World!\n";
+
+
+
+
+  
+}
