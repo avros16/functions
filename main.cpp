60bef5b5760fa6bf1db89b61867fa19e93bba0bf
avros16 <19015561-avros16@users.noreply.replit.com>
2024-04-01 13:03:14 +0000

    power/exponents

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <cmath> 
+
+using namespace std; 
+
 int main() {
 
 // functions always have parenthasis. Paramteres go inside the (). 
   // floor(5.2) = 5
   // ceil(5.2) = 6
   // have to include lib cmath
+  // cout << pow(5,4) (this is the exponoents to 5 to the power of 4)
   
   std::cout << "Hello World!\n";
 
