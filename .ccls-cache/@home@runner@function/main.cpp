26a5ad7fe6206d1dc45332def4e6696a8a24e9b2
avros16 <19015561-avros16@users.noreply.replit.com>
2024-04-01 12:59:38 +0000

    init

--- .ccls-cache/@home@runner@function/main.cpp
+++ .ccls-cache/@home@runner@function/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
+#include <cmath> 
 int main() {
 
 // functions always have parenthasis. Paramteres go inside the (). 
   // floor(5.2) = 5
   // ceil(5.2) = 6
+  // have to include lib cmath
   
   std::cout << "Hello World!\n";
 
