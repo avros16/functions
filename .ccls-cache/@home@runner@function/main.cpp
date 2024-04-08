0bd764d21274cef7c670c4d3232588fda5c45d77
avros16 <114757828+avros16@users.noreply.github.com>
2024-04-08 13:12:14 +0000

    example 1

--- .ccls-cache/@home@runner@function/main.cpp
+++ .ccls-cache/@home@runner@function/main.cpp
@@ -2,54 +2,44 @@
 #include <cmath> 
 
 using namespace std; 
-//delcare a function but define under main VV
-// -- void  welcome_message();
 
+int gettingprice(int tickets) {
+int price{0}; 
+int age{0};
+int counter{1};
 
-
-
-//takes and gives
-int ret_sq (int num) {
-  int sq{0};
-  sq= num*num; 
-  
-return sq;
+while (counter <= tickets) {
+     
+     cout << "What is the age for ticket number " << counter << " "; 
+     cin >> age;
+ counter ++;
+     if (age > 5) {
+       price += 5;
+     } else if (age <5) {
+       price += 2;
+     }
+   }
+ 
+   return price;
 }
-
-
-//defining a function. void cauuse it doesnt give anything. no input no output function
-void welcome_message(){
-  
-  cout <<"welcome to csci 271";
-  
-}
-
-
-//input, no output
-void add_func(int num1, int num2) {
-  
-int sum{0};
-sum = num1 + num2;
-  cout <<"The value of their sum is " << sum;
-  
-}
-
-
 
 
  int main() {
 
-int catch_sq;
    
-   //call function 
-   welcome_message(); // semicolon
-   int num1 =5;
-   int num2 =3;
-   //call function
-   add_func(num1,num2);
-   //call function 
-   catch_sq = ret_sq(4);
-   cout << catch_sq; 
+
+int tickets{0};
+int final_price{0};
+   
+
+
+   cout << "Hello, how many tickets would you like? ";
+   cin >> tickets; 
+   
+   final_price = gettingprice(tickets);
+   cout << "Final price is: " << final_price;
+
+
    
  
 }
