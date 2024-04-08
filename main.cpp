54a27a79a2b7f829d13ff1af5a151d0ebe7eaa5a
avros16 <114757828+avros16@users.noreply.github.com>
2024-04-08 12:29:52 +0000

    function 2

--- main.cpp
+++ main.cpp
@@ -4,12 +4,21 @@
 using namespace std; 
 
 
-//defining a function. void cauuse it doesnt give anything
+//defining a function. void cauuse it doesnt give anything. no input no output function
 void welcome_message(){
   
   cout <<"welcome to csci 271";
   
 }
+
+
+//input, no output
+void add_func(int num1, int num2) {
+
+  
+}
+
+
 
 
  int main() {
