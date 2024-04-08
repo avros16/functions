1af69ea0891ad16bd2f945a1b212b092dca3df1b
avros16 <114757828+avros16@users.noreply.github.com>
2024-04-08 12:17:52 +0000

    notes and functions

---
+++ .ccls-cache/@home@runner@function/notes.cpp
@@ -0,0 +1,36 @@
+// add return type of the function. int, float, string etc. Does not return anything, return void. this will not print
+// void message () {
+//    cout << "this is a test message" << endl;
+// }
+
+//   int count{0};
+
+//   while (count <= 5 ) {
+//   message ();
+//     count++;
+//   }
+
+
+
+// functions always have parenthasis. Paramteres go inside the (). 
+  // floor(5.2) = 5
+  // ceil(5.2) = 6
+  // have to include lib cmath
+  // cout << pow(5,4) (this is the exponoents to 5 to the power of 4)
+  // cout <<log10(10) 
+
+
+
+
+// 4/8/2024 *******
+
+
+//inputs and outputs are optional. 
+//functions come with (), inputs go inside the (). srand(time(null)) - inputs/parameters
+// have to include package <cmath>
+   //. for a function you need:
+// 1. Function defintion - define function. take this, output that etc
+   // 2. function calling - call the function 
+
+   //functions should be out of main
+
--- main.cpp
+++ main.cpp
@@ -3,34 +3,13 @@
 
 using namespace std; 
 
-// add return type of the function. int, float, string etc. Does not return anything, return void. this will not print
-void message () {
-   cout << "this is a test message" << endl;
+
+//defining a function
+void welcome_message(){
+  
 }
 
-
-int main() {
-
-  int count{0};
-  
-  while (count <= 5 ) {
-  message ();
-    count++;
-  }
+ int main() {
 
 
-  
-// functions always have parenthasis. Paramteres go inside the (). 
-  // floor(5.2) = 5
-  // ceil(5.2) = 6
-  // have to include lib cmath
-  // cout << pow(5,4) (this is the exponoents to 5 to the power of 4)
-  // cout <<log10(10) 
-  
-
-
-
-// 4/8/2024 *******
-
-  
 }
